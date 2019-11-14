0298ce59da81ddd7c453a12d9d6a4f811164efda
Admin <you@example.com>
2019-11-14 18:11:35 +0530

    Added useracase4 and run till position is 100 and set position=0 when negative

--- snakesandladder.sh
+++ snakesandladder.sh
@@ -3,22 +3,33 @@ position=0;
 ladder=1;
 noplay=0;
 snake=2;
+
 function die()
 {
 random=$(( RANDOM%6 ))
 
 }
-die
 function play()
 {
 random1=$(( RANDOM%3 )) 
 
  case $random1 in $noplay )
-        position=0;;
+        position=$(( $position + 0 ));;
                $ladder )
         position=$(( $position + $random ));;
                $snake )
-        position=$(( $position + $random ));;
+        position=$(( $position - $random ));;
+       
  esac
+if [ $position -lt 0 ]
+then
+position=0
+
+ fi
 }
-play
+
+while [ $position -lt 100 ]
+do
+   die
+   play
+done
