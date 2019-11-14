bed9e2a4b7df6ed5b3b3addf7fbd0726187fca69
Admin <you@example.com>
2019-11-14 18:35:05 +0530

    Added usercase6 for printing no of dies and pos after every die

--- snakesandladder.sh
+++ snakesandladder.sh
@@ -3,11 +3,12 @@ position=0;
 ladder=1;
 noplay=0;
 snake=2;
-
+allPosition=0;
+totaldie=0;
 function die()
 {
 random=$(( RANDOM%6 ))
-
+totaldie=$(( $totaldie+$random ))
 }
 function play()
 {
@@ -19,7 +20,7 @@ random1=$(( RANDOM%3 ))
         position=$(( $position + $random ));;
                $snake )
         position=$(( $position - $random ));;
-       
+           
  esac
  if [ $position -lt 0 ]
     then
