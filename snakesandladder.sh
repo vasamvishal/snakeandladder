5f6a09af473d7ade35cadf76a1e3995f64043051
Admin <you@example.com>
2019-11-14 17:30:57 +0530

    Added usercase3 and created a case for options

--- snakesandladder.sh
+++ snakesandladder.sh
@@ -1,8 +1,24 @@
 #Welcome to snakes and ladder
 position=0;
-
+ladder=1;
+noplay=0;
+snake=2;
 function die()
 {
 random=$(( RANDOM%6 ))
+
 }
 die
+function play()
+{
+random1=$(( RANDOM%3 )) 
+
+ case $random1 in $noplay )
+        position=0;;
+               $ladder )
+        position=$(( $position + $random ));;
+               $snake )
+        position=$(( $position + $random ));;
+ esac
+}
+play
